bb253ee9326721dcee6cdfdbac83a72c77b64cbd
santiago1708 <vela123tique@gmail.com>
2024-09-18 08:57:49 -0500

    Actualizacion: Colores

--- src/components/CalorieDisplay.tsx
+++ src/components/CalorieDisplay.tsx
@@ -7,7 +7,7 @@ type CalorieDisplayProps = {
 export default function CalorieDisplay({calorie, text, color} : CalorieDisplayProps) {
     return (
         <p className="text-white font-bold rounded-lg grid grid-cols-1 gap-3 text-center">
-                <span className={`font-black text-6xl text-${color}-500`}>{calorie}</span>
+                <span className={`font-black text-6xl text-${color}-500 `}>{calorie}</span>
                 {text}
         </p>
     )
